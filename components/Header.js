83c18833df0fab33ede4d5f7279204d2923cf7a8
Anubhav <aadhikari1248@gmail.com>
2023-07-02 13:41:42 +0530

    test

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <footer className="nav">
             {/* <img src="../images/photo-grid.png" className="hero--photo" /> */}
-            <h1 className="header">✈️ My travel journal</h1>
+            <h1 className="header">✈️ My travel journal hehe</h1>
         
         </footer>
     )
